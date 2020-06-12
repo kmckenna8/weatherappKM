15df0c4e75b2306a3caa83430a17e82e15750178
kmckenna8 <65576231+kmckenna8@users.noreply.github.com>
2020-06-12 12:53:27 -0400

    added code

---
+++ src/script.js
@@ -0,0 +1,53 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayArray = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = dayArray[date.getDay()];
+
+  return `${day}, ${hours}:${minutes}`;
+}
+let currentDate = document.querySelector("#date");
+let currentTime = new Date();
+currentDate.innerHTML = formatDate(currentTime);
+
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchInput.value}`;
+  url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+let form = document.querySelector("#search-form");
+
+form.addEventListener("submit", searchCity);
+
+// weather
+
+let apiKey = "b45bff57c0a4b94d2c90e9e31a3b7385";
+
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+function showWeather(response) {
+  console.log(response.data.main.temp);
+  let temperature = Math.round(response.data.main.temp);
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${temperature}°C`;
+}
+
+axios.get(url).then(showWeather);
